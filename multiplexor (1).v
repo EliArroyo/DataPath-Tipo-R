5c0ecdfa8ec72763d8f52102bb35e7f4951c0a80
RebeHdez <karla.hernandez9197@alumnos.udg.mx>
2025-04-30 22:57:34 -0600

    Update multiplexor (1).v

--- multiplexor (1).v
+++ multiplexor (1).v
@@ -7,22 +7,22 @@ Ernesto Lopez
 UdeG CUCEI ICOM*/
 
 module multiplexor(
-    input wire[31:0]entrada1_mux,
-    input wire[31:0]entrada2_mux,
-    input wire mux_sel,
-    output reg[31:0]salida_mux
+    input wire[31:0]in1mux,
+    input wire[31:0]in2mux,
+    input wire muxsel,
+    output reg[31:0]outmux
 );
 
 always@(*) 
 	begin
-		if(mux_sel)
+		if(muxsel)
 		begin
-			salida_mux = entrada1_mux;  // La salida toma el valor de la entrada 1.
+			outmux = in1mux;  // La salida toma el valor de la entrada 1.
 		end
 		
 		else
 		begin
-			salida_mux = entrada2_mux;  // La salida toma el valor de la entrada 2.
+			outmux = in2mux;  // La salida toma el valor de la entrada 2.
 		end
 	end
 endmodule
