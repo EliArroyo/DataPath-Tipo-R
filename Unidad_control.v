eb7cfa92132720ffd539cb60e1ebfa84b514a42e
RebeHdez <karla.hernandez9197@alumnos.udg.mx>
2025-04-30 22:56:42 -0600

    Update Unidad_control.v

--- Unidad_control.v
+++ Unidad_control.v
@@ -6,25 +6,23 @@ Ernesto Lopez
 27/04/2025
 UdeG CUCEI ICOM*/
 
-module Unidad_control(
+module UnidadControl(
     input wire [5:0]  OPcode,
-    output reg [5:0]  salida_control
+    output reg [5:0]  outControl
 );
-
-//Salida control bits:
-//5      RegWrite -> BR
-//4 3 2  ALUcontrol
-//1		 MemToWrite -> MemDatos
-//0		 MemToReg	-> Multiplexor
 
 always@(*) 
 	begin
 		case (OPcode)
-			6'b000000:  salida_control = 6'b1_010_0_0;		// Opcode de instrucciones tipo R
-			6'b000001:  salida_control = 6'b0_010_0_0;		// Opcode de instrucciones tipo R
-			default: salida_control = 6'd0;       			// Por defecto, salida en 0
+			6'b000000:  outControl = 6'b1_010_0_0;		// Opcode de instrucciones tipo R
+			6'b000001:  outControl = 6'b0_010_0_0;		// Opcode de instrucciones tipo R
+			default: outControl = 6'd0;       			// Por defecto, salida en 0
 		endcase
 	end
 endmodule
 
-
+/*Salida control de bits
+5      	 RegWrite -> BR
+4 3 2 	 ALUcontrol
+1		 MemToWrite -> MemDatos
+0		 MemToReg	-> Multiplexor*/
