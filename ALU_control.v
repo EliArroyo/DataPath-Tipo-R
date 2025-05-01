9f75de8312c5e5ea4ab0e4ad66313c2832fd1e6b
RebeHdez <karla.hernandez9197@alumnos.udg.mx>
2025-04-30 22:55:23 -0600

    Update ALU_control.v

--- ALU_control.v
+++ ALU_control.v
@@ -6,27 +6,26 @@ Ernesto Lopez
 27/04/2025
 UdeG CUCEI ICOM*/
 
-module ALU_control(
+module ALUControl(
     input wire [5:0]  Function,
-    input wire [2:0]  ALUOp_in,
-    output reg [2:0]  OpALU_out
+    input wire [2:0]  ALUOpin,
+    output reg [2:0]  OpALUout
 );
 
 always@(*) 
 	begin
-		case (ALUOp_in)
+		case (ALUOpin)
 			3'b010:									// Instrucciones tipo R
 				case (Function)
-					6'b100000: OpALU_out = 3'b010;		//Suma
-					6'b100010: OpALU_out = 3'b110;		//Resta
-					6'b100100: OpALU_out = 3'b000;		//AND
-					6'b100101: OpALU_out = 3'b001;		//OR
-					6'b101010: OpALU_out = 3'b111;		//SLT
+					6'b100000: OpALUout = 3'b010;		//Suma
+					6'b100010: OpALUout = 3'b110;		//Resta
+					6'b100100: OpALUout = 3'b000;		//AND
+					6'b100101: OpALUout = 3'b001;		//OR
+					6'b101010: OpALUout = 3'b111;		//SLT
 				endcase
-			3'b001: OpALU_out = 3'd0;        			// Instrucciones tipo J
-			3'b000: OpALU_out = 3'd0;       			// Instrucciones tipo I
-			default: OpALU_out = 3'd0;       			// Por defecto, salida en 0
+			3'b001: OpALUout = 3'd0;        			// Instrucciones tipo J
+			3'b000: OpALUout = 3'd0;       			// Instrucciones tipo I
+			default: OpALUout = 3'd0;       			// Por defecto, salida en 0
 		endcase
 	end
 endmodule
-
